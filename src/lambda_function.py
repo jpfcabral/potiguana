5fe1ed3e0c5d1abbe35c68db32a15c0e1db03c20
jpfcabral <jpfcabral@gmail.com>
2025-01-11 18:31:23 -0300

    return error

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,24 +1,25 @@
 import json
 
 import requests
+from loguru import logger
 
 from config.settings import settings
 from services.chatbot_service import ChatbotService
 
 
 def lambda_handler(event, context):
-    chatbot = ChatbotService()
     try:
+        logger.info("Init telegram request")
+        chatbot = ChatbotService()
         body = json.loads(event["body"])
+        chat_id = body["message"]["chat"]["id"]
 
-        print(body)
+        logger.info(f"Message body: {body}")
 
         message_part = body["message"].get("text")
         print("Message part : {}".format(message_part))
 
         response = chatbot.answer(query=message_part)
-
-        chat_id = body["message"]["chat"]["id"]
 
         url = f"https://api.telegram.org/bot{settings.TELEGRAM_API_TOKEN}/sendMessage"
         payload = {"chat_id": chat_id, "text": response["content"]}
@@ -26,5 +27,11 @@ def lambda_handler(event, context):
         r = requests.post(url, json=payload, timeout=10)
 
         return {"statusCode": 200}
-    except:
-        return {"statusCode": 500}
+    except Exception as exc:
+        logger.error(exc)
+        return {
+            "statusCode": 500,
+            "error": str(exc),
+            "chat_id": chat_id,
+            "text": "Erro",
+        }
