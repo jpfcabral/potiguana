5afd34e2857c1c5298048004ee5ae04fe6ac4447
João Pedro F Cabral <48861830+jpfcabral@users.noreply.github.com>
2025-01-17 10:14:09 -0300

    init semantic routing

--- src/services/chatbot_service.py
+++ src/services/chatbot_service.py
@@ -56,10 +56,10 @@ class ChatbotService:
         route: str = self.semantic_router.get_route(query)
 
         if route == "greetings":
-            return {"response": "Olá, eu sou a potiguana"}
+            return {"content": "Olá, eu sou a potiguana"}
 
         if route == "farewells":
-            return {"response": "Até logo"}
+            return {"content": "Até logo"}
 
         # Retrieve and rerank contexts
         context_docs = self.vector_store.search(query=query)
