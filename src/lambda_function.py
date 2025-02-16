1fcd489816027733552d78be8d8d1d9d0bf2919e
jpfcabral <jpfcabral@gmail.com>
2025-02-16 00:05:10 -0300

    add telegram view

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,61 +1,12 @@
-import json
-import traceback
-
 from loguru import logger
 
-from config.texts import GREETING
-from services.chatbot_service import ChatbotService
-from services.telegram_service import TelegramService
+from views.telegram_view import TelegramView
 
-chatbot = ChatbotService()
-telegram_service = TelegramService()
-
-
-def answer_message(body: dict):
-    message_part = body["message"].get("text")
-
-    if message_part == "/start":
-        telegram_service.send_message(body=body, message=GREETING, add_suggestions=True)
-        return {"statusCode": 200}
-
-    response = chatbot.answer(query=message_part)
-    telegram_service.send_message(
-        body=body,
-        message=response["content"],
-        ask_feedback=True if "route" not in response else False,
-        add_suggestions=True if response.get("route", None) == "greetings" else False,
-    )
-
-    return {"statusCode": 200}
-
-
-def answer_callback_query(body: dict):
-    callback_id = body["callback_query"]["id"]
-    telegram_service.answer_callback_query(id=callback_id)
-    telegram_service.save_callback(body=body)
-
-    return {"statusCode": 200}
+telegram_view = TelegramView()
 
 
 def lambda_handler(event, context):
     logger.info(f"Received event: {event}")
     logger.info(f"Received context: {context}")
 
-    try:
-        body = json.loads(event["body"])
-
-        if "message" in body:
-            return answer_message(body=body)
-
-        if "callback_query" in body:
-            return answer_callback_query(body=body)
-
-        raise Exception(f"Unknown request: {body}")
-    except Exception as exc:
-        logger.error(exc)
-        logger.error(f"Detalhes do traceback:\n {traceback.format_exc()}")
-        return {
-            "statusCode": 200,
-            "error": str(exc),
-            "text": "Erro",
-        }
+    return telegram_view.awnser_message(event)
---
+++ src/views/telegram_view.py
@@ -0,0 +1,61 @@
+import json
+import traceback
+
+from loguru import logger
+
+from config.texts import GREETING
+from services.chatbot_service import ChatbotService
+from services.telegram_service import TelegramService
+
+chatbot = ChatbotService()
+telegram_service = TelegramService()
+
+
+class TelegramView:
+    def awnser_message(self, event):
+        try:
+            body = json.loads(event["body"])
+
+            if "message" in body:
+                return self.answer_message(body=body)
+
+            if "callback_query" in body:
+                return self.answer_callback_query(body=body)
+
+            raise Exception(f"Unknown request: {body}")
+        except Exception as exc:
+            logger.error(exc)
+            logger.error(f"Detalhes do traceback:\n {traceback.format_exc()}")
+            return {
+                "statusCode": 200,
+                "error": str(exc),
+                "text": "Erro",
+            }
+
+    def answer_message(self, body: dict):
+        message_part = body["message"].get("text")
+
+        if message_part == "/start":
+            telegram_service.send_message(
+                body=body, message=GREETING, add_suggestions=True
+            )
+            return {"statusCode": 200}
+
+        response = chatbot.answer(query=message_part)
+        telegram_service.send_message(
+            body=body,
+            message=response["content"],
+            ask_feedback=True if "route" not in response else False,
+            add_suggestions=True
+            if response.get("route", None) == "greetings"
+            else False,
+        )
+
+        return {"statusCode": 200}
+
+    def answer_callback_query(self, body: dict):
+        callback_id = body["callback_query"]["id"]
+        telegram_service.answer_callback_query(id=callback_id)
+        telegram_service.save_callback(body=body)
+
+        return {"statusCode": 200}
