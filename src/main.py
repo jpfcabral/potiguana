f29c8a1ec724f5341f337c0ea4c7ef9819f3ca89
jpfcabral <jpfcabral@gmail.com>
2025-03-08 22:43:25 -0300

    add local testing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from services.chatbot_service import ChatbotService
@@ -10,3 +11,7 @@ def chat(query: str):
     """Triggers chatbot pipeline"""
     chatbot = ChatbotService()
     return chatbot.answer(query=query)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
